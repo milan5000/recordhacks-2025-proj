deb0a7360bdaba19f0ee5a8d709cd3d9cc50977d
milan5000 <milan5000 on GitHub>
2025-03-29 22:23:23 -0500

    Bare-bones frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState, useEffect } from 'react'
+
 const App = () => {
+  const [supplies, setSupplies] = useState([
+    {
+      id: 1,
+      name: 'Canned Food',
+      available: true
+    },
+    {
+      id: 2,
+      name: 'Bottled Water',
+      available: true
+    },
+    {
+      id: 3,
+      name: '$25-100 or more',
+      available: true
+    }
+  ])
+  const [avail, setAvail] = useState({
+    water: true,
+    food: true,
+    first_aid_kit: true
+  })
+
   return (
     <div>
-      <p>Hello, world!</p>
+      <h1>PackAdvisor</h1>
+      <div>
+        {supplies.map(supply =>
+          <div key={supply.id}>
+            {supply.name} {supply.available ? "Available" : "Unavailable"}
+            <button>Change availability (TODO)</button>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
