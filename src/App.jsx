38e59dcb64a460580ff77990edd083fb78aa389c
milan5000 <milan5000 on GitHub>
2025-03-30 10:34:51 -0500

    Create FilteredSupplyList functionality, add index.css

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ const App = () => {
     packingListService
       .getAll()
       .then(initialSupplies => {
-        console.log(initialSupplies['essential'])
-        setSupplies(initialSupplies['essential'])
+        console.log(initialSupplies['packing_list'])
+        setSupplies(initialSupplies['packing_list'])
       })
   }, [])
 
@@ -30,6 +30,18 @@ const App = () => {
     const situClone = { ...situParams };
     situClone[arg] = val;
     setSituParams(situClone)
+  }
+
+  const toggleSupplyAvail = (arg) => {
+    console.log(`Toggling availability of ${arg}`)
+    const supplyClone = structuredClone(supplies)
+    console.log(supplyClone)
+    const indexToUpdate = supplyClone.findIndex(obj => obj.id === arg)
+    if (indexToUpdate !== -1) {
+      supplyClone[indexToUpdate].available = !supplyClone[indexToUpdate].available
+      console.log(`Toggled ${arg} to ${supplyClone[indexToUpdate].available}`)
+    }
+    setSupplies(supplyClone)
   }
 
   return (
@@ -41,7 +53,7 @@ const App = () => {
         <button onClick={() => {console.log("education"); setMode("education")}}>Educational</button>
       </div>
       <SituationSelect mode={mode} situParams={situParams} editParam={editParam} />
-      <FilteredSupplyList mode={mode} situParams={situParams} />
+      <FilteredSupplyList mode={mode} situParams={situParams} supplies={supplies} toggleSupplyAvail={toggleSupplyAvail}/>
     </div>
   )
 }
--- src/components/FilteredSupplyList.jsx
+++ src/components/FilteredSupplyList.jsx
@@ -1,12 +1,17 @@
-const FilteredSupplyList = ({ mode, situParams }) => {
+const FilteredSupplyList = ({ mode, situParams, supplies, toggleSupplyAvail }) => {
     if (mode === null) {
         return null
-    } else if (mode !== disaster) {
+    } else if (mode !== "disaster") {
         return <div></div> // currently unimplemented
-    } else if (mode === disaster) {
+    } else if (mode === "disaster") {
         return (
             <div>
-                
+                {supplies.map(supply =>
+                <div key={supply.id}>
+                    {supply.name} {supply.available ? "Available" : "Unavailable"}
+                    <button onClick={() => toggleSupplyAvail(supply.id)}>Change availability (TODO)</button>
+                </div>
+                )}
             </div>
         )
     }
